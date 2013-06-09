ab5044cbe34cb46954510f24fdd423428293352b
didcoppa@gmail.com <didcoppa@gmail.com@e78347fa-2565-ec12-d170-10d114ef2833>
2013-06-09 20:32:20 +0000

    de cero de nuevo

---
+++ trunk/ proyecto-squash-5-utnfrc/test/unit/proyecto/JugadorTests.groovy
@@ -0,0 +1,17 @@
+package proyecto
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Jugador)
+class JugadorTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
