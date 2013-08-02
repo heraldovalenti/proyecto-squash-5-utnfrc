bedb4fd4595aa5df7c4052bbcd6bf409b3b567cc
didcoppa@gmail.com <didcoppa@gmail.com@e78347fa-2565-ec12-d170-10d114ef2833>
2013-08-02 21:49:54 +0000

    agregue la inscripcion a torneo

---
+++ trunk/grails-app/domain/modelo/InscripcionTorneo.groovy
@@ -0,0 +1,19 @@
+package modelo
+
+
+class InscripcionTorneo {
+
+		Torneo torneo
+		Date fechaInscripcion
+		boolean inscripto
+   
+	
+	String toString()
+	{
+		return torneo +" "+inscripto
+	}
+
+	static constraints = {
+		torneo Nullable:false		
+	}
+}
