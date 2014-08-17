804127917b2253dac51bd0352191b2df998dd182
heraldovalenti@gmail.com <heraldovalenti@gmail.com@e78347fa-2565-ec12-d170-10d114ef2833>
2014-08-17 16:05:40 +0000

    Automatización de cambio de estado de torneos finalizada. Cambios automáticos: - desde "creado" a "inscripcion abierta" - desde "inscripcion abierta" a "inscripcion cerrada" Tests de integración creados.

--- trunk/test/integration/sgt/TorneoServiceTests.groovy
+++ trunk/test/integration/sgt/TorneoServiceTests.groovy
@@ -11,32 +11,96 @@ class TorneoServiceTests {
 	@Test
 	void testAbrirInscripcionesTorneo() {
 		Date hoy = new Date()
-		Date antes = new Date(hoy.getTime() - (1000 * 60 * 60 * 24))
+		Date aperturaInscripcion = new Date(hoy.getTime() - (1000 * 60 * 60 * 24)) //la fecha de hoy MENOS un dia
+		Date clausuraInscripcion = new Date(hoy.getTime() + (1000 * 60 * 60 * 24)) //la fecha de hoy MAS un dia
 		Torneo unTorneo = new Torneo(
 				nombre: "Torneo 1",
-				fechaAlta: antes,
-				fechaInicioInscripcion: antes,
-				fechaFinInscripcion: antes,
-				fechaInicioTorneo: antes,
-				fechaFinTorneo: antes,
+				fechaAlta: hoy,
+				fechaInicioInscripcion: aperturaInscripcion,
+				fechaFinInscripcion: clausuraInscripcion,
+				fechaInicioTorneo: hoy,
+				fechaFinTorneo: hoy,
 				club: null,
 				estado: "Creado",
 				puntuable: false
 				);
 		unTorneo.save(failOnError: true)
 		assertTrue(unTorneo.creado())
-		torneoService.abrirInscripcionesTorneo()
+		torneoService.actualizarEstados()
+		assertTrue(unTorneo.inscripcionAbierta())
+	}
+	
+	@Test
+	void testCerrarInscripcionesTorneo() {
+		Date hoy = new Date()
+		Date clausuraInscripcion = new Date(hoy.getTime() - (1000 * 60 * 60 * 24)) //la fecha de hoy MENOS un dia
+		Torneo unTorneo = new Torneo(
+				nombre: "Torneo 1",
+				fechaAlta: hoy,
+				fechaInicioInscripcion: hoy,
+				fechaFinInscripcion: clausuraInscripcion,
+				fechaInicioTorneo: hoy,
+				fechaFinTorneo: hoy,
+				club: null,
+				estado: "Inscripcion Abierta",
+				puntuable: false
+				);
+		unTorneo.save(failOnError: true)
+		assertTrue(unTorneo.inscripcionAbierta())
+		torneoService.actualizarEstados()
+		assertTrue(unTorneo.inscripcionCerrada())
+	}
+	
+	@Test
+	void testTorneoCreadoPeroInscripcionNoParaAbrirTodavia() {
+		Date hoy = new Date()
+		Date aperturaInscripcion = new Date(hoy.getTime() + (1000 * 60 * 60 * 24)) //la fecha de hoy MAS un dia
+		Torneo unTorneo = new Torneo(
+				nombre: "Torneo 1",
+				fechaAlta: hoy,
+				fechaInicioInscripcion: aperturaInscripcion,
+				fechaFinInscripcion: hoy,
+				fechaInicioTorneo: hoy,
+				fechaFinTorneo: hoy,
+				club: null,
+				estado: "Creado",
+				puntuable: false
+				);
+		unTorneo.save(failOnError: true)
+		assertTrue(unTorneo.creado())
+		torneoService.actualizarEstados()
+		assertTrue(unTorneo.creado())
+	}
+	
+	@Test
+	void testTorneoInscripcionAbiertaPeroInscripcionNoParaCerrarTodavia() {
+		Date hoy = new Date()
+		Date clausuraInscripcion = new Date(hoy.getTime() + (1000 * 60 * 60 * 24)) //la fecha de hoy MAS un dia
+		Torneo unTorneo = new Torneo(
+				nombre: "Torneo 1",
+				fechaAlta: hoy,
+				fechaInicioInscripcion: hoy,
+				fechaFinInscripcion: clausuraInscripcion,
+				fechaInicioTorneo: hoy,
+				fechaFinTorneo: hoy,
+				club: null,
+				estado: "Inscripcion Abierta",
+				puntuable: false
+				);
+		unTorneo.save(failOnError: true)
+		assertTrue(unTorneo.inscripcionAbierta())
+		torneoService.actualizarEstados()
 		assertTrue(unTorneo.inscripcionAbierta())
 	}
 	
     @Before
     void setUp() {
-        // Setup logic here
+        
     }
 
     @After
     void tearDown() {
-        // Tear down logic here
+        
     }
 
 }
