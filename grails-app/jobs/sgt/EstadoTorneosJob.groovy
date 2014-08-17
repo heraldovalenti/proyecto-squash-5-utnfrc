1aefe2268d0c8aa063df39ff09ee07d3c103833a
heraldovalenti@gmail.com <heraldovalenti@gmail.com>
2014-08-17 14:57:19 +0000

    Cambio de estado de torneo automatizado.

---
+++ grails-app/jobs/sgt/EstadoTorneosJob.groovy
@@ -0,0 +1,15 @@
+package sgt
+
+class EstadoTorneosJob {
+    
+	def torneoService
+	
+	static triggers = {
+		simple name: "EstadoTorneoSimple", startDelay:10000l, repeatInterval: 10000l, repeatCount: 1
+		cron name: "EstadoTorneoCron", startDelay: 10000l, cronExpression: "0 5 0 * * ?"
+    }
+
+    def execute() {
+        torneoService.actualizarEstados()
+    }
+}
---
+++ grails-app/services/sgt/TorneoService.groovy
@@ -0,0 +1,38 @@
+package sgt
+
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+class TorneoService {
+	
+	def actualizarEstados() {
+		abrirInscripcionesTorneo()
+		/*cerrarInscripcionesTorneo()*/
+	}
+
+    def abrirInscripcionesTorneo() {
+		Date hoy = new Date()
+		List<Torneo> torneosCreados = Torneo.findAll()
+		for (torneo in torneosCreados) {
+			println "torneo.nombre=" + torneo.nombre
+			println "torneo.estado(before)=" + torneo.estado
+			if (torneo.fechaInicioInscripcion.before(hoy)) {
+				torneo.abrirInscripcion()
+				torneo.save(failOnError: true)
+			}
+			println "torneo.estado(after)=" + torneo.estado
+		}
+    }
+	
+	
+	/*def cerrarInscripcionesTorneo() {
+		Date hoy = new Date()
+		List<Torneo> inscripcionesAbiertas = Torneo.findAllByEstado("Inscripcion Abierta")
+		for (torneo in inscripcionesAbiertas) {
+			if (torneo.fechaFinInscripcion.before(hoy)) {
+				torneo.cerrarInscripcion()
+				torneo.save(failOnError: true)
+			}
+		}
+	}*/
+}
