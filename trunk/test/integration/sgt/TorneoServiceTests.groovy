07af2152012dea54368d1646918fd59cd02e8b74
heraldovalenti@gmail.com <heraldovalenti@gmail.com@e78347fa-2565-ec12-d170-10d114ef2833>
2014-08-17 14:57:19 +0000

    Cambio de estado de torneo automatizado.

---
+++ trunk/test/integration/sgt/TorneoServiceTests.groovy
@@ -0,0 +1,42 @@
+package sgt
+
+import static org.junit.Assert.*
+import org.junit.*
+
+class TorneoServiceTests {
+
+	static transactional = true	
+	def torneoService
+	
+	@Test
+	void testAbrirInscripcionesTorneo() {
+		Date hoy = new Date()
+		Date antes = new Date(hoy.getTime() - (1000 * 60 * 60 * 24))
+		Torneo unTorneo = new Torneo(
+				nombre: "Torneo 1",
+				fechaAlta: antes,
+				fechaInicioInscripcion: antes,
+				fechaFinInscripcion: antes,
+				fechaInicioTorneo: antes,
+				fechaFinTorneo: antes,
+				club: null,
+				estado: "Creado",
+				puntuable: false
+				);
+		unTorneo.save(failOnError: true)
+		assertTrue(unTorneo.creado())
+		torneoService.abrirInscripcionesTorneo()
+		assertTrue(unTorneo.inscripcionAbierta())
+	}
+	
+    @Before
+    void setUp() {
+        // Setup logic here
+    }
+
+    @After
+    void tearDown() {
+        // Tear down logic here
+    }
+
+}
