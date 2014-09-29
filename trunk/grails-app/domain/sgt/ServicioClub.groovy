f1477141d98e688a00803103896c5661f8cd44cd
heraldovalenti@gmail.com <heraldovalenti@gmail.com@e78347fa-2565-ec12-d170-10d114ef2833>
2014-09-29 03:41:35 +0000

    Mejorada funcionalidad de gestion de servicios de club.

--- trunk/grails-app/domain/sgt/ServicioClub.groovy
+++ trunk/grails-app/domain/sgt/ServicioClub.groovy
@@ -3,12 +3,24 @@ package sgt
 class ServicioClub {
 	
 	String nombre
-	String descripcion
-	
-	static belongsTo = Club
 
     static constraints = {
-		nombre blank:false, maxSize: 50
-		descripcion blank: false, maxSize: 250
+		nombre blank: false, maxSize: 50, inList: ["Sanitarios","Cantina",
+			"Restaurant","Estacionamiento","Guarderia","Lockers","Duchas",
+			"Kiosko","Sport shopping", "Tienda de recuerdos"]
     }
+	
+	@Override
+	public int hashCode() {
+		return nombre.hashCode()
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof ServicioClub)) {
+			return false
+		}
+		ServicioClub other = o;
+		return this.nombre.equals(other.nombre)
+	}
 }
--- trunk/grails-app/views/club/_menuClub.gsp
+++ trunk/grails-app/views/club/_menuClub.gsp
@@ -6,7 +6,7 @@
 			<li><g:link controller="club" action="showDomicilio">Domicilio</g:link></li>
 			<li><g:link controller="encargado" action="index">Encargados de club</g:link></li>
 			<li><g:link controller="cancha" action="list" namespace="club">Gestion de canchas</g:link></li>
-			<li><g:link controller="servicioClub" action="list">Gestion de servicios ofrecidos</g:link></li>
+			<li><g:link controller="club" action="serviciosClub">Servicios de club</g:link></li>
 			<li><g:link controller="postulacionTorneo" action="index">Postulaciones a torneos</g:link></li>
 		</ul>
 	</div>
