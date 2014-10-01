3844955c521d97ccf5cd850dddd5480636f035ff
fankguillermo@gmail.com <fankguillermo@gmail.com@e78347fa-2565-ec12-d170-10d114ef2833>
2014-10-01 22:56:12 +0000

    Se agrego la taglib de la imagen y saco el fondo de color del div para la card.

---
+++ grails-app/taglib/sgt/ImageTagLib.groovy
@@ -0,0 +1,20 @@
+package sgt
+
+class ImageTagLib {	
+	
+def filesService
+	
+def imagenPerfilJugador={ attrs ->
+	
+	def usuario=attrs.usuario
+		
+	def dir= filesService.retrieveImagesDir()
+	def img= "default.jpg"
+	if(usuario.jugador && usuario.jugador.imagen){
+		img=usuario.jugador.imagen.nombre
+	}	
+	out << g.resource (dir:dir,file:img)
+
+
+}
+}
