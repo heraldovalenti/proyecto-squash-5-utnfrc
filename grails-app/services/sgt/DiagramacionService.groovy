8e909e6081e7a64d73c89cf85d7784ddbf881944
mati.dc89@gmail.com <mati.dc89@gmail.com@e78347fa-2565-ec12-d170-10d114ef2833>
2014-10-16 23:16:15 +0000

    Agregado de métodos para organizacion de cruces o enfrentamientos

--- grails-app/services/sgt/DiagramacionService.groovy
+++ grails-app/services/sgt/DiagramacionService.groovy
@@ -2,15 +2,13 @@ package sgt
 
 import java.util.Scanner;
 
-
-
 class DiagramacionService {
 	
 		
 	/*
      * Calcula la cantidad de Rondas a jugar en  base a la cantidad de Jugadores Inscriptos
      */
-    int calcularRondas(){
+    int calcularRondas(Jugador[] jugadores){
         double cantJug=jugadores.length
 		int cantRondas=0
         while(cantJug>1)
@@ -22,13 +20,47 @@ class DiagramacionService {
     }
 	
 	/*
+	 * Rellena con Jugadores vacios
+	 */
+	Jugador [] generarJugadoresVacios(Jugador[] jugadores1,Categoria categoria){
+		Jugador [] jugadoresDestino;
+		Jugador [] jugadores=this.ordenarJugadores(jugadores1, categoria)
+		int maxJugPriRonda= (int)Math.pow(2, calcularRondas(jugadores1));
+		jugadoresDestino=new Jugador[maxJugPriRonda];
+		int maxPos=0;
+		for (int i=0;i<jugadores.length;i++)
+		{
+			jugadoresDestino[i] = jugadores[i];
+			if(jugadores[i].getPosicionRankingCategoria(categoria)()>=maxPos){
+				maxPos=jugadores[i].getPosicionRankingCategoria(categoria)+2;
+			}
+		}
+		
+		for (int i=jugadores.length;i<jugadoresDestino.length;i++)
+		{
+			
+			jugadoresDestino[i]=new Jugador(maxPos);
+			jugadoresDestino[i].setVacio(true);
+			maxPos++;
+		}
+		return jugadoresDestino;
+	}
+	
+	
+	
+	/*
 	 * Ordena Jugadores con su posicion
 	 */
 	Jugador[] ordenarJugadores(Jugador[] jugadores,Categoria categoria){
 		Jugador aux;
 		for (int i = 0; i < jugadores.length - 1; i++) {
 			for (int x = i + 1; x < jugadores.length; x++) {
-				if (jugadores[x].getPosicionRankingCategoria(categoria) < jugadores[i].getPosicionRankingCategoria(categoria)) {
+				if (jugadores[x].getPosicionRankingCategoria(categoria) == 0) {
+					aux = jugadores[i];
+					jugadores[i] = jugadores[x];
+					jugadores[x] = aux;
+				}
+			    else if (jugadores[x].getPosicionRankingCategoria(categoria) < jugadores[i].getPosicionRankingCategoria(categoria)) {
 					aux = jugadores[i];
 					jugadores[i] = jugadores[x];
 					jugadores[x] = aux;
@@ -36,6 +68,21 @@ class DiagramacionService {
 			}
 		}
 		return jugadores
+	}
+	
+	/*
+	 * Cruza Jugadores con su posicion
+	 */
+	Partido [] cruzarJugadores(Jugador[] jugadores1,Categoria categoria){
+		int maxJugPriRonda= (int)Math.pow(2, calcularRondas(jugadores1));
+		int cantPart=maxJugPriRonda/2;
+		Partido [] partidosPrimeraRonda=new Partido[cantPart];
+		Jugador [] jugadores= this.generarJugadoresVacios(jugadores1, categoria)
+		for (int i=0;i<partidosPrimeraRonda.length;i++)
+		{
+			partidosPrimeraRonda[i]=new Partido(jugadores[i],jugadores[jugadores.length-i-1]);
+		}
+		return partidosPrimeraRonda
 	}
 
 }
