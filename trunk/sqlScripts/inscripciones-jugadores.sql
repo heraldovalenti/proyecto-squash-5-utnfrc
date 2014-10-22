2fbc821afc99ebbb8fe331f90b7907bcdb792491
heraldovalenti@gmail.com <heraldovalenti@gmail.com@e78347fa-2565-ec12-d170-10d114ef2833>
2014-10-22 21:55:15 +0000

    Test para el servicio de diagramacion.

--- trunk/sqlScripts/inscripciones-jugadores.sql
+++ trunk/sqlScripts/inscripciones-jugadores.sql
@@ -1,5 +1,6 @@
 /* REQUERIDOS: TORNEOS */
 
+/*inscripciones de jugadores de categoria primera*/
 insert into inscripcion_torneo (id,usuario_id,detalle_torneo_id,estado,fecha,version) values
 (1, 1, 1, 'Vinculada','2014-01-01',1),
 (2, 2, 1, 'Vinculada','2014-01-01',1),
@@ -8,3 +9,30 @@ insert into inscripcion_torneo (id,usuario_id,detalle_torneo_id,estado,fecha,ver
 (5, 12, 1, 'Vinculada','2014-01-01',1),
 (6, 13, 1, 'Vinculada','2014-01-01',1),
 (7, 14, 1, 'Vinculada','2014-01-01',1);
+
+/* inscripciones jugadores de categoria segunda */
+insert into inscripcion_torneo (id,usuario_id,detalle_torneo_id,estado,fecha,version) values
+/*jugadores rankeados*/
+(8, 15, 2, 'Vinculada','2014-01-01',1),
+(9, 16, 2, 'Vinculada','2014-01-01',1),
+(10,17, 2, 'Vinculada','2014-01-01',1),
+(11,18, 2, 'Vinculada','2014-01-01',1),
+/*jugadores no rankeados*/
+(12,19, 2, 'Vinculada','2014-01-01',1),
+(13,20, 2, 'Vinculada','2014-01-01',1),
+(14,21, 2, 'Vinculada','2014-01-01',1);
+
+/*inscripciones de jugadores de categoria tercera*/
+insert into inscripcion_torneo (id,usuario_id,detalle_torneo_id,estado,fecha,version) values
+(15, 1, 3, 'Vinculada','2014-01-01',1),
+(16, 2, 3, 'Vinculada','2014-01-01',1),
+(17, 3, 3, 'Vinculada','2014-01-01',1),
+(18, 4, 3, 'Vinculada','2014-01-01',1),
+(19, 20, 3, 'Vinculada','2014-01-01',1),
+(20, 21, 3, 'Vinculada','2014-01-01',1),
+(21, 22, 3, 'Vinculada','2014-01-01',1),
+(22, 23, 3, 'Vinculada','2014-01-01',1),
+(23, 24, 3, 'Vinculada','2014-01-01',1),
+(24, 25, 3, 'Vinculada','2014-01-01',1),
+(25, 26, 3, 'Vinculada','2014-01-01',1),
+(26, 27, 3, 'Vinculada','2014-01-01',1);
