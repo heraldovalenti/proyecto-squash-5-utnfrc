9b71f0eda0f81cac01da0662257c2270e7129c2d
mati.dc89@gmail.com <mati.dc89@gmail.com@e78347fa-2565-ec12-d170-10d114ef2833>
2014-10-26 22:03:43 +0000

    Clases de diagramacion de horarios de torneos

--- src/java/logica/DisponibilidadHoraria.java
+++ src/java/logica/DisponibilidadHoraria.java
@@ -4,41 +4,158 @@
  */
 package logica;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.TreeSet;
+import java.util.ArrayList;
 
 /**
  *
  * @author Matias
  */
-public class DisponibilidadHoraria {
+public class DisponibilidadHoraria implements Comparable{
     
-    TreeSet <Horario>[] horarios= new TreeSet [7];
-        
+    private ArrayList <Horario>[][] horarios;
+    private boolean[][] matrizVerdad;
+
+    public DisponibilidadHoraria(int horas, int dias) {
+        horarios= new ArrayList [horas][dias];
+        matrizVerdad=new boolean[horas][dias];
+        for (ArrayList<Horario>[] horario : horarios) {
+            for (ArrayList<Horario> horario1 : horario) {
+                horario1 = new <Horario>ArrayList();
+            }
+        }
+    }
+
+    public boolean[][] getMatrizVerdad() {
+        return matrizVerdad;
+    }
+
+    public void setMatrizVerdad(boolean[][] matrizVerdad) {
+        this.matrizVerdad = matrizVerdad;
+    }
 
 
     public DisponibilidadHoraria() {
-        for(int i =0;i<horarios.length;i++)
-        {
-            horarios[i]=new TreeSet <Horario>();
+        horarios= new ArrayList [16][7];
+        matrizVerdad=new boolean[16][7];
+        for (ArrayList<Horario>[] horario : horarios) {
+            for (ArrayList<Horario> horario1 : horario) {
+                horario1 = new <Horario>ArrayList();
+            }
         }
     }
 
-    public TreeSet <Horario>[] getHorarios() {
+    public ArrayList<Horario>[][] getHorarios() {
         return horarios;
     }
 
-    public void setHorarios(TreeSet <Horario>[] horarios) {
+    public void setHorarios(ArrayList<Horario>[][] horarios) {
         this.horarios = horarios;
     }
+    
+    /**
+     * Cualquier elemento con disponibilidadHoraria carga su horario
+     * @param elementoDisponible elemento con disponibilidadHoraria
+     * @param hora hora disponible
+     * @param diaSemana dia de a semana disponible(ya sea lunes, martes, etc)
+     */
+    public void cargarHorario(IDisponible elementoDisponible,int hora, int diaSemana){
+        Horario h =new Horario(hora, diaSemana, elementoDisponible);
+        horarios[hora][diaSemana].add(h);
+    }
+    
+    public boolean[][] generarMatrizVerdad(){
+        for(int i = 0; i < horarios.length; i++){ 
+            for (int j = 0; j < horarios[i].length; j++) {
+                 matrizVerdad[i][j]=!horarios[i][j].isEmpty(); } }
+        return matrizVerdad;
+    }
+    
+    public boolean disponible(int hora, int diaSemana){
+        return matrizVerdad[hora][diaSemana];
+    }
+    
+    /**
+     * cruza disponibilidades horarias dejando solo los q ambos estan disponibles
+     * @param matrizVerdad
+     * @return 
+     */
+    public boolean[][]CruzarDisponibilidadSemanalNegativa(boolean[][] matrizVerdad){
+        for(int i = 0; i < horarios.length; i++){ 
+            for (int j = 0; j < horarios[i].length; j++) {
+                 matrizVerdad[i][j]=this.matrizVerdad[i][j] && matrizVerdad[i][j]; } }
+        return matrizVerdad;
+    }
+    
+    public void generarDisponibilidadConMatrizVerdad(boolean[][] matrizVerdad, IDisponible elementoDisp){
+        for(int i = 0; i < horarios.length; i++){ 
+            for (int j = 0; j < horarios[i].length; j++) {
+                 if(matrizVerdad[i][j]){
+                 horarios[i][j].add(new Horario(i, j, elementoDisp));}
+                     } }
+    }
 
-    public void mostrarHorarios(int ordenDia) {
-        System.out.println(" Lista de " + horarios[ordenDia].size() +
-        " horarios");
-        for( Iterator it = horarios[ordenDia].iterator(); it.hasNext();) {
-            Horario h = (Horario)it.next();
-            System.out.println(h);
+    public void add(DisponibilidadHoraria disponibilidad) {
+        ArrayList <Horario>[][] dispHorarios=disponibilidad.getHorarios();
+        for(int i = 0; i < dispHorarios.length; i++){ 
+            for (int j = 0; j < dispHorarios[i].length; j++) {
+                if(!dispHorarios[i][j].isEmpty()) {
+                    for(int x=0;x<dispHorarios[i][j].size();x++){
+                         horarios[i][j].add(new Horario(i, j, dispHorarios[i][j].get(x).getElementoDisponibilidad()));
+                    }
+                }
+            }
         }
     }
+    
+    public int contarDisponibilidades(){
+        int count=0;
+        for (ArrayList<Horario>[] horario : horarios) {
+            for (ArrayList<Horario> horario1 : horario) {
+                count += horario1.size();
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (this.contarDisponibilidades()==0)return 1;
+        DisponibilidadHoraria externo=(DisponibilidadHoraria)o;
+        if (externo.contarDisponibilidades()==0)return -1;
+        return this.contarDisponibilidades()-externo.contarDisponibilidades();
+    }
+    
+    public ArrayList <Horario> obtenerHorarioMenorDisponiblidad(boolean[][]matrizValor){
+        ArrayList <Horario> h=null;
+        for(int i=0;i<matrizValor.length;i++){
+                for(int j=0;j<matrizValor[i].length;j++){
+                    if(matrizValor[i][j]){
+                        if(h==null){
+                            h=horarios[i][j];
+                        }
+                        if(h.size()>horarios[i][j].size()){
+                            h=horarios[i][j];
+                        }
+                    }
+                }
+            }
+        return h;
+    }
+    
+    public void eliminarHorariosEnfrentamiento(boolean[][]matrizValor,IDisponible elemento){
+        for(int i=0;i<matrizValor.length;i++){
+            for(int j=0;j<matrizValor[i].length;j++){                    
+                if(matrizValor[i][j]){                        
+                    Horario h=elemento.getDisponibilidadHoraria().getHorarios()[i][j].get(0);                        
+                    if(this.horarios[i][j].contains(h)){                            
+                        this.horarios[i][j].remove(h);                        
+                    }
+                }
+            }
+        }
+                
+    }
+    
+
+
 }
--- src/java/logica/Torneo.java
+++ src/java/logica/Torneo.java
@@ -4,10 +4,189 @@
  */
 package logica;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Matias
  */
 public class Torneo {
+    Club organizador;
+    Partido[]enfrentamientos;
+    DisponibilidadHoraria disponibilidadEnfrentamientos=new DisponibilidadHoraria();
+    int dias=7,horas=16;
+    ArrayList <HorarioCanchaPartido>[][] horarioDefinitivoTorneo;
+    private int[][] matrizConteoClub;
+
+    public Partido[] getEnfrentamientos() {
+        return enfrentamientos;
+    }
+
+    public void setEnfrentamientos(Partido[] enfrentamientos) {
+        this.enfrentamientos = enfrentamientos;
+    }
+
+    public ArrayList<HorarioCanchaPartido>[][] getHorarioDefinitivoTorneo() {
+        return horarioDefinitivoTorneo;
+    }
+
+    public void setHorarioDefinitivoTorneo(ArrayList<HorarioCanchaPartido>[][] horarioDefinitivoTorneo) {
+        this.horarioDefinitivoTorneo = horarioDefinitivoTorneo;
+    }
+
+    public Torneo(){
+        horarioDefinitivoTorneo= new ArrayList [horas][dias];
+        matrizConteoClub= new int[horas][dias];
+        for (ArrayList<HorarioCanchaPartido>[] horario : horarioDefinitivoTorneo) {
+            for (ArrayList<HorarioCanchaPartido> horario1 : horario) {
+                horario1 = new <HorarioCanchaPartido>ArrayList();
+            }
+        }
+    }
     
+    public Torneo(int horas,int dias,Club organizador,Partido[]enfrentamientos){
+        this.horas=horas;
+        this.dias=dias;
+        this.organizador = organizador;
+        this.enfrentamientos=enfrentamientos;
+        horarioDefinitivoTorneo= new ArrayList [horas][dias];
+        matrizConteoClub= new int[horas][dias];
+        for (ArrayList<HorarioCanchaPartido>[] horario : horarioDefinitivoTorneo) {
+            for (ArrayList<HorarioCanchaPartido> horario1 : horario) {
+                horario1 = new <HorarioCanchaPartido>ArrayList();
+            }
+        }
+    }
+    
+    
+
+    public Torneo(Club organizador) {
+        this.organizador = organizador;
+        for (ArrayList<HorarioCanchaPartido>[] horario : horarioDefinitivoTorneo) {
+            for (ArrayList<HorarioCanchaPartido> horario1 : horario) {
+                horario1 = new <HorarioCanchaPartido>ArrayList();
+            }
+        }
+    }
+
+    public Club getOrganizador() {
+        return organizador;
+    }
+
+    public void setOrganizador(Club organizador) {
+        this.organizador = organizador;
+    }
+    
+    public void generarDiagramacion(){
+        generarDisponibilidadEnfrentamientos();
+        generarDisponibilidadEnfrentamientosEnCanchas();
+        generarMatrizConteo();
+        generarHorarioTorneo();
+    }
+    
+    //1 genera enfrentamientos en los horarios (toda la semana)
+    public void generarDisponibilidadEnfrentamientos(){
+        for (Partido enfrentamiento : enfrentamientos) {
+            enfrentamiento.generarDisponibilidadPartido();
+            disponibilidadEnfrentamientos.add(enfrentamiento.getDisponibilidadHoraria());
+        }
+    }
+    
+    //2 saca los enfrentamientos en los que no hay canchas disponibles (toda la semana)
+    public void generarDisponibilidadEnfrentamientosEnCanchas(){
+        boolean[][] matrizVerdadClub=organizador.disponibilidadHoraria.generarMatrizVerdad();
+        for(int i = 0; i < matrizVerdadClub.length; i++){ 
+            for (int j = 0; j < matrizVerdadClub[i].length; j++) {
+                 if(!matrizVerdadClub[i][j]){ 
+                     if(!disponibilidadEnfrentamientos.getHorarios()[i][j].isEmpty())disponibilidadEnfrentamientos.getHorarios()[i][j].removeAll(disponibilidadEnfrentamientos.getHorarios()[i][j]);
+                     for (Partido enfrentamiento : enfrentamientos) {
+                        enfrentamiento.generarDisponibilidadPartido();
+                        if(!enfrentamiento.disponibilidadHoraria.getHorarios()[i][j].isEmpty())enfrentamiento.disponibilidadHoraria.getHorarios()[i][j].removeAll(enfrentamiento.disponibilidadHoraria.getHorarios()[i][j]);
+                     }
+                 }
+            }
+        }
+    }
+    
+    //3 genera la cantidad de canchas disponibles por horario
+    public void generarMatrizConteo(){
+        boolean[][] matrizVerdadTorneo=disponibilidadEnfrentamientos.generarMatrizVerdad();
+        for(int i = 0; i < matrizVerdadTorneo.length; i++){ 
+            for (int j = 0; j < matrizVerdadTorneo[i].length; j++) {
+                 if(matrizVerdadTorneo[i][j]){ 
+                     matrizConteoClub[i][j]=organizador.disponibilidadHoraria.getHorarios()[i][j].size();
+                 }
+            }
+        }
+    }
+    
+    //resta contador de canchas, si es cero no hay mas canchas disponibles en eso horario y cambia la matriz de verdad del club
+    public void tomarCancha(int i,int j){
+        matrizConteoClub[i][j]--;
+        if(matrizConteoClub[i][j]==0){
+            organizador.disponibilidadHoraria.getMatrizVerdad()[i][j]=false;
+            sacarHorarioEnPartidos(i, j);
+        }
+    }
+    
+    //quita el horario en los partidos, si es cero no hay mas canchas disponibles en eso horario saca el horario de los partidos
+    public void sacarHorarioEnPartidos(int i,int j){
+        for (Partido enfrentamiento : enfrentamientos) {
+            ArrayList<Horario> arregloHorario = enfrentamiento.getDisponibilidadHoraria().getHorarios()[i][j];
+            if (!arregloHorario.isEmpty()) {
+                arregloHorario.removeAll(arregloHorario);
+            }
+        }
+    }
+    
+    /**
+     * obtenemos la posicion en el array del enfrentamiento con menor disp horaria
+     * @return 
+     */
+    public int obtenerPosEnfrentamientoMenorDisponibilidad()
+    {
+        int pos=0;
+        Partido menor=enfrentamientos[pos];
+        for(int i=1;i<enfrentamientos.length;i++) {
+            if ((!enfrentamientos[i].isAsignado())&&enfrentamientos[i].getDisponibilidadHoraria().compareTo(menor.getDisponibilidadHoraria())<0) {
+                menor=enfrentamientos[i];
+                pos=i;
+            }
+        }
+        if(enfrentamientos[pos].getDisponibilidadHoraria().contarDisponibilidades()==0)
+        {
+            return -1;
+        }
+        return pos;
+    }
+    
+    public void eliminarHorariosEnfrentamiento(Partido p){
+        
+        boolean[][]matrizVerdadPartido=p.getDisponibilidadHoraria().generarMatrizVerdad();
+        this.disponibilidadEnfrentamientos.eliminarHorariosEnfrentamiento(matrizVerdadPartido, p);
+    }
+        
+    
+    public void generarHorarioTorneo(){
+        int pos=obtenerPosEnfrentamientoMenorDisponibilidad();
+        while(pos>=0){
+            enfrentamientos[pos].setAsignado(true);
+            DisponibilidadHoraria horario=enfrentamientos[pos].getDisponibilidadHoraria();
+            boolean[][]matrizVerdadPartido=horario.generarMatrizVerdad();
+            
+            ArrayList <Horario> h=disponibilidadEnfrentamientos.obtenerHorarioMenorDisponiblidad(matrizVerdadPartido );
+            int hora=h.get(0).getHoraInicio();
+            int diaSemana=h.get(0).getOrdenDia();
+            
+            ArrayList<Horario> horarioCanchas=organizador.getDisponibilidadHoraria().getHorarios()[hora][diaSemana];
+            Cancha canchaAsignada=(Cancha)horarioCanchas.remove(0).getElementoDisponibilidad();
+            HorarioCanchaPartido horarioDefinitivo=new HorarioCanchaPartido(hora,diaSemana,canchaAsignada,enfrentamientos[pos]);
+            
+            tomarCancha(hora,diaSemana);
+            eliminarHorariosEnfrentamiento(enfrentamientos[pos]);
+            
+            horarioDefinitivoTorneo[hora][diaSemana].add(horarioDefinitivo);
+            pos=obtenerPosEnfrentamientoMenorDisponibilidad();
+        }
+    }
 }
