4f3029bd96eeb3fbaee3097f42f126cbeeb1a2b7
heraldovalenti@gmail.com <heraldovalenti@gmail.com@e78347fa-2565-ec12-d170-10d114ef2833>
2014-11-26 22:52:01 +0000

    Inscripciones de jugadores a I Fecha puntuable 2014

--- trunk/scripts/inscripciones-jugadores.sql
+++ trunk/scripts/inscripciones-jugadores.sql
@@ -1,59 +1,64 @@
 /* REQUERIDOS: TORNEOS */
 
-/*inscripciones de jugadores de categoria primera*/
-insert into inscripcion_torneo (id,usuario_id,detalle_torneo_id,estado,fecha,version) values
-(1, 1, 1, 'Vinculada','2014-01-01',1),
-(2, 2, 1, 'Vinculada','2014-01-01',1),
-(3, 3, 1, 'Vinculada','2014-01-01',1),
-(4, 4, 1, 'Vinculada','2014-01-01',1),
-(5, 12, 1, 'Vinculada','2014-01-01',1),
-(6, 13, 1, 'Vinculada','2014-01-01',1),
-(7, 14, 1, 'Vinculada','2014-01-01',1);
-
-/* inscripciones jugadores de categoria segunda */
-insert into inscripcion_torneo (id,usuario_id,detalle_torneo_id,estado,fecha,version) values
-/*jugadores rankeados*/
-(8, 15, 2, 'Vinculada','2014-01-01',1),
-(9, 16, 2, 'Vinculada','2014-01-01',1),
-(10,17, 2, 'Vinculada','2014-01-01',1),
-(11,18, 2, 'Vinculada','2014-01-01',1),
-/*jugadores no rankeados*/
-(12,19, 2, 'Vinculada','2014-01-01',1),
-(13,20, 2, 'Vinculada','2014-01-01',1),
-(14,21, 2, 'Vinculada','2014-01-01',1);
-
-/*inscripciones de jugadores de categoria tercera*/
-insert into inscripcion_torneo (id,usuario_id,detalle_torneo_id,estado,fecha,version) values
-(15, 1, 3, 'Vinculada','2014-01-01',1),
-(16, 2, 3, 'Vinculada','2014-01-01',1),
-(17, 3, 3, 'Vinculada','2014-01-01',1),
-(18, 4, 3, 'Vinculada','2014-01-01',1),
-(19, 20, 3, 'Vinculada','2014-01-01',1),
-(20, 21, 3, 'Vinculada','2014-01-01',1),
-(21, 22, 3, 'Vinculada','2014-01-01',1),
-(22, 23, 3, 'Vinculada','2014-01-01',1),
-(23, 24, 3, 'Vinculada','2014-01-01',1),
-(24, 25, 3, 'Vinculada','2014-01-01',1),
-(25, 26, 3, 'Vinculada','2014-01-01',1),
-(26, 27, 3, 'Vinculada','2014-01-01',1);
-
-insert into inscripcion_torneo (id,usuario_id,detalle_torneo_id,estado,fecha,version) values
-(27, 1, 19, 'Vinculada','2014-01-01',1),
-(28, 2, 19, 'Vinculada','2014-01-01',1),
-(29, 3, 19, 'Vinculada','2014-01-01',1),
-(30, 4, 19, 'Vinculada','2014-01-01',1),
-(31, 12, 19, 'Vinculada','2014-01-01',1),
-(32, 13, 19, 'Vinculada','2014-01-01',1),
-(33, 1, 20, 'Vinculada','2014-01-01',1),
-(34, 2, 20, 'Vinculada','2014-01-01',1),
-(35, 3, 20, 'Vinculada','2014-01-01',1),
-(36, 4, 20, 'Vinculada','2014-01-01',1),
-(37, 12, 20, 'Vinculada','2014-01-01',1),
-(38, 1, 21, 'Vinculada','2014-01-01',1),
-(39, 2, 21, 'Vinculada','2014-01-01',1),
-(40, 3, 21, 'Vinculada','2014-01-01',1),
-(41, 4, 21, 'Vinculada','2014-01-01',1),
-(42, 12, 21, 'Vinculada','2014-01-01',1),
-(43, 13, 21, 'Vinculada','2014-01-01',1),
-(44,14, 21, 'Vinculada','2014-01-01',1);
-
+insert into inscripcion_torneo (id,usuario_id,detalle_torneo_id,estado,fecha,version) 
+values 
+#categoria 1
+(1,1,1,'Vinculada','2014-01-01',1),
+(2,2,1,'Vinculada','2014-01-01',1),
+(3,3,1,'Vinculada','2014-01-01',1),
+(4,4,1,'Vinculada','2014-01-01',1),
+(5,5,1,'Vinculada','2014-01-01',1),
+(6,6,1,'Vinculada','2014-01-01',1),
+(7,7,1,'Vinculada','2014-01-01',1),
+(8,8,1,'Vinculada','2014-01-01',1),
+(9,9,1,'Vinculada','2014-01-01',1),
+#categoria 2
+(10,10,2,'Vinculada','2014-01-01',1),
+(11,11,2,'Vinculada','2014-01-01',1),
+(12,12,2,'Vinculada','2014-01-01',1),
+(13,13,2,'Vinculada','2014-01-01',1),
+(14,14,2,'Vinculada','2014-01-01',1),
+(15,15,2,'Vinculada','2014-01-01',1),
+(16,16,2,'Vinculada','2014-01-01',1),
+(17,17,2,'Vinculada','2014-01-01',1),
+(18,18,2,'Vinculada','2014-01-01',1),
+(19,19,2,'Vinculada','2014-01-01',1),
+(20,20,2,'Vinculada','2014-01-01',1),
+#categoria 3
+(21,21,3,'Vinculada','2014-01-01',1),
+(22,22,3,'Vinculada','2014-01-01',1),
+(23,23,3,'Vinculada','2014-01-01',1),
+(24,24,3,'Vinculada','2014-01-01',1),
+(25,25,3,'Vinculada','2014-01-01',1),
+(26,26,3,'Vinculada','2014-01-01',1),
+(27,27,3,'Vinculada','2014-01-01',1),
+(28,28,3,'Vinculada','2014-01-01',1),
+(29,29,3,'Vinculada','2014-01-01',1),
+(30,30,3,'Vinculada','2014-01-01',1),
+(31,31,3,'Vinculada','2014-01-01',1),
+(32,32,3,'Vinculada','2014-01-01',1),
+(33,33,3,'Vinculada','2014-01-01',1),
+# Cuarta
+(34, 34, 4, 'Vinculada','2014-01-01',1),
+(35, 35, 4, 'Vinculada','2014-01-01',1),
+(36, 35, 4, 'Vinculada','2014-01-01',1),
+(37, 37, 4, 'Vinculada','2014-01-01',1),
+(38, 38, 4, 'Vinculada','2014-01-01',1),
+(39, 39, 4, 'Vinculada','2014-01-01',1),
+(40, 40, 4, 'Vinculada','2014-01-01',1),
+(41, 41, 4, 'Vinculada','2014-01-01',1),
+(42, 42, 4, 'Vinculada','2014-01-01',1),
+(43, 43, 4, 'Vinculada','2014-01-01',1),
+(44, 44, 4, 'Vinculada','2014-01-01',1),
+# Quinta
+(45, 45, 5, 'Vinculada','2014-01-01',1),
+(46, 46, 5, 'Vinculada','2014-01-01',1),
+(47, 47, 5, 'Vinculada','2014-01-01',1),
+(48, 48, 5, 'Vinculada','2014-01-01',1),
+(49, 49, 5, 'Vinculada','2014-01-01',1),
+# Damas A
+(50, 50, 8, 'Vinculada','2014-01-01',1),
+(51, 51, 8, 'Vinculada','2014-01-01',1),
+(52, 52, 8, 'Vinculada','2014-01-01',1),
+(53, 53, 8, 'Vinculada','2014-01-01',1),
+(54, 54, 8, 'Vinculada','2014-01-01',1);
