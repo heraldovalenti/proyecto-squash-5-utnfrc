7ba759f6d002484e30cd85708310ef9d97167914
heraldovalenti@gmail.com <heraldovalenti@gmail.com@e78347fa-2565-ec12-d170-10d114ef2833>
2014-11-26 17:04:14 +0000

    Script de ranking de jugadores actualizado.

--- trunk/scripts/rankings-jugadores.sql
+++ trunk/scripts/rankings-jugadores.sql
@@ -1,6 +1,6 @@
 /* REQUERIDOS: JUGADORES */
 
-/* Ranking para jugadores de categoria A - Damas */
+/* Primera - Masculinos */
 insert into ranking (id,version,jugador_id,categoria_id,puesto,puntaje) values
 (1,1,1,1,1,1000),
 (2,1,2,1,2, 900),
@@ -8,48 +8,52 @@ insert into ranking (id,version,jugador_id,categoria_id,puesto,puntaje) values
 (4,1,4,1,4, 700),
 (5,1,5,1,5, 600),
 (6,1,6,1,6, 500),
-(7,1,7,1,7, 400);
+(7,1,7,1,7, 400),
+(8,1,8,1,8, 300),
+(9,1,9,1,9, 200);
 
-/* Ranking para jugadores de categoria 1 (Primera) */
+/* Segunda - Masculinos */
 insert into ranking (id,version,jugador_id,categoria_id,puesto,puntaje) values
-(1,1,1,1,1,1000),
-(2,1,2,1,2, 900),
-(3,1,3,1,3, 800),
-(4,1,4,1,4, 700),
-(5,1,5,1,5, 600),
-(6,1,6,1,6, 500),
-(7,1,7,1,7, 400);
-/* Ranking para jugadores de categoria 2 (Segunda): 7 Jugadores, pero solo 4 rankeados */
-insert into ranking (id,version,jugador_id,categoria_id,puesto,puntaje) values
-(8, 1,8, 2,1,1000),
-(9, 1,9, 2,2, 800),
-(10,1,10,2,3, 600),
-(11,1,11,2,4, 400);
+(10,1,10,2,1,1000),
+(11,1,11,2,2, 950),
+(12,1,12,2,3, 900),
+(13,1,13,2,4, 800),
+(14,1,14,2,5, 700),
+(15,1,15,2,6, 600),
+(16,1,16,2,7, 500),
+(17,1,17,2,8, 400),
+(18,1,18,2,9, 300),
+(19,1,19,2,10,200),
+(20,1,20,2,11,100);
 
-/* Ranking para jugadores de categoria 3 (Tercera): Solo 6 en Tercera actualmente y 20 rankeados en tercera */
-/* inicio - jugadores en tercera */
+/* Tercera - Masculinos */
 insert into ranking (id,version,jugador_id,categoria_id,puesto,puntaje) values
-(12,1,15,3 ,1,1000),
-(13,1,16,3 ,2, 950),
-(14,1,17,3 ,3, 900),
-(15,1,18,3 ,4, 850),
-(16,1,19,3 ,5, 800),
-(17,1,20,3 ,6, 750);
-/* inicio - jugadores de primera rankeados en tercera */
+(21,1,21,3,1, 950),
+(22,1,22,3,2, 900),
+(23,1,23,3,3, 800),
+(24,1,24,3,4, 700),
+(25,1,25,3,5, 600),
+(26,1,26,3,6, 500),
+(27,1,27,3,7, 400),
+(28,1,28,3,8, 300),
+(29,1,29,3,9, 200),
+(30,1,30,3,10, 50),
+(31,1,31,3,11, 40),
+(32,1,32,3,12, 25),
+(33,1,33,3,13, 10);
+
+/* Quinta - Masculinos */
 insert into ranking (id,version,jugador_id,categoria_id,puesto,puntaje) values
-/*(18,1,1 ,3 ,7, 650),
-(19,1,2 ,3 ,8, 600),
-(20,1,3 ,3 ,9, 550),
-(21,1,4 ,3 ,10, 500),*/
-(22,1,5 ,3 ,11, 450),
-(23,1,6 ,3 ,12, 400),
-(24,1,7 ,3 ,13, 350);
-/* inicio - jugadores de segunda rankeados en tercera */
+(45,1,45,5,1, 950),
+(46,1,46,5,2, 900),
+(47,1,47,5,3, 800),
+(48,1,48,5,4, 700),
+(49,1,49,5,5, 510);
+
+/* A - Femeninos */
 insert into ranking (id,version,jugador_id,categoria_id,puesto,puntaje) values
-(25,1,8 ,3 ,14, 300),
-(26,1,9 ,3 ,15, 250),
-(27,1,10,3 ,16, 200),
-(28,1,11,3 ,17, 150),
-(29,1,12,3 ,18, 100),
-(30,1,13,3 ,19,  50),
-(31,1,14,3 ,20,  20);
+(50,1,50,8,1,1000),
+(51,1,51,8,2, 900),
+(52,1,52,8,3, 800),
+(53,1,53,8,4, 700),
+(54,1,54,8,5, 600);
